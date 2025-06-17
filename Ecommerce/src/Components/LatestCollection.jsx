b168f63753e9130634062eb08201300c86c33b77
SatyaVarun29 <satyavarun87@gmail.com>
2025-06-17 09:57:42 +0530

    add

---
+++ Ecommerce/src/Components/LatestCollection.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestCollection,setLatestCollection]=useState([])
+
+  useEffect(()=>{
+     setLatestCollection(products.slice(0,10))
+  },[])
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem ipsa accusamus excepturi.
+        </p>
+      </div>
+
+      {/* rendering products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+         {latestCollection.map((item)=>
+        <ProductItem key={item.id} id={item._id} image={item.image} name={item.name} price={item.price}/>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
